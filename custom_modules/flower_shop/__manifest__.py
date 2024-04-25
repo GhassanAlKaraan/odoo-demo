1ac2ec2e525d80588fd25c08e9f85601dfe68f1c
Ghassan Al Karaan <ghass.karaan@gmail.com>
2024-04-25 16:07:08 +0300

    part5

--- custom_modules/flower_shop/__manifest__.py
+++ custom_modules/flower_shop/__manifest__.py
@@ -6,14 +6,17 @@
     'description': 'FLOWER SHOP',
     'category': 'Inventory/Inventory',
     'website': 'https://www.odoo.com',
-    'depends': ['base', 'product', 'sale', 'stock', ],  # sale --> product --> product variant
+    'depends': ['base', 'product', 'sale', 'stock', ],
     'data': [
 
         'security/ir.model.access.csv',
         'views/flower_shop_views.xml',
         'views/product_views.xml',
         'views/stock_lot_views.xml',
-        'data/ir_actions_server_data.xml'
+        'data/ir_actions_server_data.xml',
+        'reports/flower_sale_order_views.xml',
+        'reports/paper_format.xml',
+        'reports/action.xml',
         # 'views/website_sale_flower_views.xml'
     ],
     'installable': True,
